45ddb2bdb4b9a04129d8971c4a85817e10ffb3ae
Sherry Zhang <zsherry2106@gmail.com>
2022-03-03 08:37:38 -0600

    test

---
+++ pandas.py
@@ -0,0 +1,15 @@
+#Sherry Zhang
+#3/1/22
+
+#pandas
+
+import pandas as pd
+import os
+
+os.system('cls')
+
+#Create pandas dataframe
+data={'name': ['Maria', 'Peter'], 'age': [30, 20]}
+df = pd.DataFrame(data)
+
+print(df)
